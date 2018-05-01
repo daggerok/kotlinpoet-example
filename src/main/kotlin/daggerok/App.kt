9f12eccde4207586e20e31f2bc3d9e6561e7eb8d
Maksim Kostromin <daggerok@gmail.com>
2018-05-01 20:17:09 +0300

    asdfasdfasdfasdfasdfasdfadsf (C) whatthecommit.com

--- src/main/kotlin/daggerok/App.kt
+++ src/main/kotlin/daggerok/App.kt
@@ -1,17 +1,21 @@
 package daggerok
 
+import com.squareup.kotlinpoet.ARRAY
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier.VARARG
+import com.squareup.kotlinpoet.TypeName
 import sun.security.action.GetPropertyAction
 import java.io.File
+import java.nio.file.Paths
 import java.security.AccessController.doPrivileged
+import java.util.concurrent.TimeUnit
 import kotlin.text.Charsets.UTF_8
 
 fun main(args: Array<String>) {
 
   val path = System::class.java.protectionDomain?.codeSource?.location?.toURI()?.path
-      ?: File(doPrivileged(GetPropertyAction("java.io.tmpdir")), "kotlinpoet").path
+      ?: File(doPrivileged(GetPropertyAction("java.io.tmpdir"))).path
 
   val directory = File(path)
 
@@ -22,16 +26,18 @@ fun main(args: Array<String>) {
   directory.mkdirs()
 
   val fileName = "Greeter"
-  val file = File(path, "$fileName.kt")
+  val file = Paths.get(path, "daggerok", "kotlinpoet", "$fileName.kt").toFile()
 
   println("generate kotlin code into $file")
-  FileSpec.builder("", fileName)
+  FileSpec.builder("daggerok.kotlinpoet", fileName)
       .addFunction(FunSpec.builder("main")
           .addParameter("args", String::class, VARARG)
           .addStatement("println(*args.toList())")
           .build())
       .build()
       .writeTo(directory)
+
+  TimeUnit.SECONDS.sleep(1)
 
   println("\nresult:\n\n${file.readText(UTF_8)}")
 }
